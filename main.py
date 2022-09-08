15f92018cbdb81d70a96ee89db49b71ed9ad1fcd
IsaC2 <112134827+1sagit@users.noreply.github.com>
2022-09-08 22:58:59 +0000

    before deleting all scenario code

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-import scenarios.py
+import scenarios
+#where the scenarios come from
+#sets up the intro and responses to it
+right=True
+start = input
+#asks if the user wants to play and sets up a response if they say no
+while right:
+  print("type yes to start game")
+  answer=input("")
+  if answer.lower() == "yes":
+    right = False
+  else: 
+    print("try again")
+#provides the first scenario and the two paths you could take
+print(scenarios.scen.get(1))
+answer=input("")
+while answer != "a" or "b":
+  print("try again")
+  print(scenarios.scen.get(1))
+  answer=input("")
+  break
+if answer.lower() == "a":
+  print(scenarios.scen.get(2))
+elif answer.lower() == "b":
+  print(scenarios.scen.get(3))
 
-start = input("type yes to start game")
-
-if start == "yes": 
-  print('scen') #insert scenario 1 here?
-else:
-  print("try again") 

0680a6b85666a6055749d53953be22bf421f3feb
IsaC2 <112134827+1sagit@users.noreply.github.com>
2022-09-08 23:49:57 +0000

    actually works

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import scenarios
 #where the scenarios come from
 #sets up the intro and responses to it
-right=True
+right = True
 start = input
 #asks if the user wants to play and sets up a response if they say no
 while right:
-  print("type yes to start game")
-  answer=input("")
-  if answer.lower() == "yes":
-    right = False
-  else: 
-    print("try again")
+    print("type yes to start game")
+    answer = input()
+    if answer.lower() == "yes":
+        right = False
+    else:
+        print("try again")
 #provides the first scenario and the two paths you could take
 print(scenarios.scen.get(1))
-answer=input("")
-while answer != "a" or "b":
-  print("try again")
-  print(scenarios.scen.get(1))
-  answer=input("")
-  break
-if answer.lower() == "a":
-  print(scenarios.scen.get(2))
-elif answer.lower() == "b":
-  print(scenarios.scen.get(3))
-
+answer = input().lower()
+while answer != "a" and answer != "b":
+    print("try again")
+    print(scenarios.scen.get(1))
+    answer = input().lower()
+if answer == "a":
+    print(scenarios.scen.get(2))
+elif answer == "b":
+    print(scenarios.scen.get(3))

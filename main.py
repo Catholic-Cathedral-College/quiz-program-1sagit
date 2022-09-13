cf3367db2ebc938148bff8c6cd33c61c8c9eee25
IsaC2 <112134827+1sagit@users.noreply.github.com>
2022-09-13 21:40:16 +0000

    added answers dict

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ while right:
     print("type yes to start game")
     answer = input()
     if answer.lower() == "yes":
+        print("\n tip: to select the choice, type the letter assigned to it or else \n") 
         right = False
     else:
         print("try again")
 #provides the first scenario and the two paths you could take
 print(scenarios.scen.get(1))
 answer = input().lower()
-while answer != "a" and answer != "b":
-    print("try again")
+while answer != "a" and answer != "b": #if the answer is wrong, it provides this result, if not it leads to next scenario
+    print("try again") 
     print(scenarios.scen.get(1))
     answer = input().lower()
-if answer == "a":
-    print(scenarios.scen.get(2))
+if answer == "a": #if you choose this choice you get this specific scenario
+    print(scenarios.ans.get("1a"))
 elif answer == "b":
-    print(scenarios.scen.get(3))
+    print(scenarios.scen.get("1b"))
--- scenarios.py
+++ scenarios.py
@@ -1,5 +1,10 @@
 scen = {
-  1: "You're in class doing your work in the back row when all of a sudden a zombie crashes in and jumps on your teacher, both falling behind the table.\n People panic some run towards your teacher but don't know how to help while others gather in groups and start talking, some pull out their phones and start recording. The room is filled with sounds of struggle. There's another door at the back of the classroom where you could escape or you could try and get the zombie off your teacher \n a. escape \n b. help your teacher" ,
+  1: "You're in class doing your work in the back row when all of a sudden a zombie crashes in and jumps on your teacher, both falling behind the table.\n People panic some run towards your teacher but don't know how to help while others gather in groups and start talking, some pull out their phones and start recording. The room is filled with sounds of struggle. Do you help your teacher and restore peace in the class? \n a. yes \n b. no" ,
   2: "asdsahdsaj" ,
   3: "wqehuwq"
+}
+
+ans = {
+  "1a": "asdsahdsaj",
+  "1b": "ioewjife"
 }
